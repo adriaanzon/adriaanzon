43534a9f43a26488900749842fe6cf6d6e14f39c
Adriaan Zonnenberg <adriaanzonn@gmail.com>
2024-03-03 21:40:55 +0100

    Add back RemapKeys as system-wide daemon

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,9 +1,3 @@
--- Remap § to `
-hs.hotkey.bind({}, "§", function() hs.eventtap.keyStroke({}, "`") end )
-hs.hotkey.bind({ "cmd" }, "§", function() hs.eventtap.keyStroke({ "cmd" }, "`") end )
-hs.hotkey.bind({ "shift" }, "§", function() hs.eventtap.keyStroke({ "shift" }, "`") end )
-hs.hotkey.bind({ "cmd", "shift" }, "§", function() hs.eventtap.keyStroke({ "cmd", "shift" }, "`") end )
-
 -- Switch menu bar clock to analog when only the MacBook's internal display is active.
 local function toggleClockDisplay()
     local screens = hs.screen.allScreens()
